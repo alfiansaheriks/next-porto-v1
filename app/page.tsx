0430bd8730c3841b94caad2af3b0ad8c26ccb6a3
alfiansaheriks <alfiansah.erik.ft20@mail.umy.ac.id>
2024-10-08 02:03:29 +0700

    added SEO on layout

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,8 @@ import { getCookie, deleteCookie, setCookie } from "@/utils/cookie";
 import { Button } from "@/components/ui/button";
 import { Icon } from "@iconify/react";
 import { useRouter } from "next/navigation";
+import { Metadata } from "next";
+
 
 export default function Home() {
   const router = useRouter();
